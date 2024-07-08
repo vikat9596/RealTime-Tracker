3de436e272737de2244402b2442a7abf5a2fbba1
Vikat Rane <vikat9596@gmail.com>
2024-07-09 02:40:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const http = require("http");
+const path = require('path');
+const socketio = require("socket.io");
+
+const server =http.createServer(app);
+
+const io=socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+        io.emit("receive-location",{id: socket.id, ...data});
+    })
+    
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect");
+    })
+})
+
+app.get("/" , function(req,res){
+    res.render("index");
+})
+
+server.listen(3000);
